37e657fd56a5b73167acbcda6c580d0445197e61
danielsv <daniels.vasconcelos@gmail.com>
2019-11-19 16:02:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php                    
-  $host= gethostname();
-  $ip = gethostbyname($host);                    
-  echo $ip;
-
 echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
